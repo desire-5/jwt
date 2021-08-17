7caa274b4a30a978aaa477abf06f3c776d41bc4e
desire-5 <vasyl.vasylkiv5@gmail.com>
2021-08-17 13:38:08 +0300

    lst mod

---
+++ client/src/App.tsx
@@ -0,0 +1,55 @@
+import React, {FC, useContext, useEffect, useState} from 'react';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {observer} from "mobx-react-lite";
+import {IUser} from "./models/IUser";
+import UserService from "./services/UserService";
+
+const App: FC = () => {
+    const {store} = useContext(Context);
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            store.checkAuth()
+        }
+    }, [])
+
+    async function getUsers() {
+        try {
+            const response = await UserService.fetchUsers();
+            setUsers(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    if (store.isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (!store.isAuth) {
+        return (
+            <div>
+                <LoginForm/>
+                <button onClick={getUsers}>Get users</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>{store.isAuth ? `User is authorized ${store.user.email}` : 'Log in'}</h1>
+            <h1>{store.user.isActivated ? 'Account confirmed by mail ' : 'Confirm account!!!!'}</h1>
+            <button onClick={() => store.logout()}>Log out</button>
+            <div>
+                <button onClick={getUsers}>Get users</button>
+            </div>
+            {users.map(user =>
+                <div key={user.email}>{user.email}</div>
+            )}
+        </div>
+    );
+};
+
+export default observer(App);
---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express');
+const cookieParser = require('cookie-parser')
+const router = require('./router/index')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const errorMiddleware = require('./middlewares/error-middleware');
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`Server run on PORT = ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
